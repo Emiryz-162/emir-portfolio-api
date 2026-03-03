dafa70f18c80e57e980b0abe61e5fe2fd0c35d0f
Muhammet Emir Yılmaz <emiryz162@gmail.com>
2026-03-03 13:56:31 +0300

    Fix CORS for production

--- EmirPortfolioAI/Program.cs
+++ EmirPortfolioAI/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.AllowAnyOrigin()
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
